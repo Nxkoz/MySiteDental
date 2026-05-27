c54d44db7cf0e5e463c105891b7e492035eccb6b
Kozachek Mykyta <kozachek_mykyta@MacBook-Air-Kozachek.local>
2026-05-27 10:28:29 +0300

    remove duplicate DATABASE_URL config

--- mysite/settings.py
+++ mysite/settings.py
@@ -143,15 +143,3 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import dj_database_url
-import os
-
-DATABASE_URL = os.environ.get('DATABASE_URL')
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600
-        )
-    }
