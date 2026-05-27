1cbcf0283441c80e970a89794610bfb59c7c5ff1
Kozachek Mykyta <kozachek_mykyta@MacBook-Air-Kozachek.local>
2026-05-27 11:55:43 +0300

    fix database settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,6 +77,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
